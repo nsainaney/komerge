35d90ed72717b828bf256eca2ee40dc785abaa93
Narayan Sainaney <narayan@sainaney.com>
2022-05-29 23:02:09 -0700

    Added args

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,41 @@ import 'source-map-support/register.js'
 import { readdir, copyFile, unlink, access } from 'fs/promises'
 import { PromisedDatabase } from 'promised-sqlite3'
 import PrettyError from 'pretty-error'
+import arg from 'arg'
 import path from 'path'
 
-const srcFolder = path.resolve(__dirname, '../samples')
-const outFile = path.resolve(__dirname, '../samples/out.sqlite')
+// const srcFolder = path.resolve(__dirname, '../samples')
+// const outFile = path.resolve(__dirname, '../samples/out.sqlite')
+// const onlyCopyTables = ['Bookmark', 'content']
 
-const renderError = (err) => {
+const parseArgs = () => {
+    try {
+        return arg({
+            '--src': String,
+            '--dst': String,
+            '--table': [String],
+
+            // Aliases
+            '-s': '--src',
+            '-d': '--dst',
+            '-t': '--table',
+        })
+    }
+    catch {
+        console.log('Options are:\n\n')
+        console.log('       --src, -s:        Folder containing source databases')
+        console.log('       --dst, -d:        Destination file to merge data into')
+        console.log('       --table, -t:      Tables to merge')
+        process.exit(0)
+    }
+}
+
+const renderError = (err: Error) => {
     const pe = new PrettyError()
     console.error(pe.render(err))
 }
 
-const exists = async (file) => {
+const exists = async (file: string) => {
     try {
         await access(file)
         return true
@@ -21,7 +45,11 @@ const exists = async (file) => {
     catch { return false }
 }
 
-const generateOutDb = async (fullPath) => {
+const toAbsolutePath = (unknownPath: string) =>  unknownPath.startsWith('/') ?
+    unknownPath :
+    path.resolve(process.cwd(), unknownPath)
+
+const generateOutDb = async (fullPath: string, outFile: string) => {
     const outExists = await exists(outFile)
     if (!outExists) {
         console.log(`Copying ${fullPath} to ${outFile}`)
@@ -35,7 +63,7 @@ const generateOutDb = async (fullPath) => {
         const tables = await dstDb.all('select name from sqlite_master where type="table"')
         return {
             continue: !outExists,
-            tables,
+            tables: tables.map(t => t.name),
             dstDb
         }
     }
@@ -53,7 +81,15 @@ const generateOutDb = async (fullPath) => {
     }
 }
 
-const main = async () => {    
+const main = async () => {
+
+    const args = parseArgs()
+    if (!args['--src']) throw new Error('missing required argument: --src')
+
+    const srcFolder = toAbsolutePath(args['--src'])
+    const outFile = toAbsolutePath(args['--dst'] || './out.sqlite')
+    const onlyCopyTables = args['--table']
+
     const files = await readdir(srcFolder)
     let dstDb: PromisedDatabase
     let index = 0
@@ -66,11 +102,10 @@ const main = async () => {
         for(const file of files) {
             const fullPath = `${srcFolder}/${file}`
             const dbName = `db${index++}`
-
             console.log(`Processing ${fullPath}`)
             try {
                 if (!dstDb) {
-                    const result = await generateOutDb(fullPath)
+                    const result = await generateOutDb(fullPath, outFile)
                     dstDb = result.dstDb
                     tables = result.tables
                     fatal = result.fatal
@@ -80,13 +115,22 @@ const main = async () => {
                     if (!dstDb) skips.push(fullPath)
                     if (result.continue) continue
                 }
+                else if (fullPath === outFile) {
+                    // Don't copy self
+                    skips.push(fullPath)
+                    console.error(`Skipping ${file}`)
+                    continue
+                }
             
                 await dstDb.run(`ATTACH '${fullPath}' AS ${dbName}`)
 
                 for(const table of tables) {
-                    const tableName = table.name
-                    console.log(`Processing table ${tableName}`)    
-                    const copyCmd = `INSERT OR IGNORE INTO ${tableName} SELECT * FROM ${dbName}.${tableName}`
+                    if (onlyCopyTables && !onlyCopyTables.includes(table)) {
+                        console.log(`Skipping table ${table}`)    
+                        continue
+                    }
+                    console.log(`Processing table ${table}`)    
+                    const copyCmd = `INSERT OR IGNORE INTO ${table} SELECT * FROM ${dbName}.${table}`
                     await dstDb.run(copyCmd)
                 }
 
@@ -100,7 +144,8 @@ const main = async () => {
             }
         }
 
-        await dstDb?.run('VACUUM') // Compact the db
+        // Compact the db
+        await dstDb?.run('VACUUM')
     }
 
     finally {
